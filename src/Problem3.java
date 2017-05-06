9e822398a72a92df2076f1ef191414c6b702d2d2
dpark3542 <dpark3542@gmail.com>
2017-05-06 11:38:13 -0400

    Problem 3 created.

---
+++ src/Problem3.java
@@ -0,0 +1,18 @@
+/**
+ * Created by dpark on 5/6/2017.
+ */
+public class Problem3 {
+    public static void main(String[] args) {
+        long x = 600851475143L;
+        long d = 2;
+        while (d <= Math.sqrt(x)) {
+            if (x % d == 0) {
+                x /= d;
+            }
+            else {
+                d++;
+            }
+        }
+        System.out.println(x);
+    }
+}
