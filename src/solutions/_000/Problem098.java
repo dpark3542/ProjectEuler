5b950f872cc115c1a3124fae47706bf3d6491fc3
Davin <dpark354@umd.edu>
2023-09-18 23:10:43 -0400

    Add problems 98 and 99

---
+++ src/solutions/_000/Problem098.java
@@ -0,0 +1,72 @@
+package solutions._000;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Problem098 {
+    private static final long LIMIT = 9876543210L;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/input/p098_words.txt"));
+        String[] a = br.readLine().split("(\\W)+");
+
+
+        long ans = 0;
+        for (int i = 0; i < a.length; i++) {
+            out: for (int j = i + 1; j < a.length; j++) {
+                int n = a[i].length();
+                if (a[j].length() != n) {
+                    continue;
+                }
+
+                int[] cnt = new int[26];
+                for (int k = 0; k < n; k++) {
+                    cnt[a[i].charAt(k) - 'A']++;
+                    cnt[a[j].charAt(k) - 'A']--;
+                }
+
+                for (int k = 0; k < 26; k++) {
+                    if (cnt[k] != 0) {
+                        continue out;
+                    }
+                }
+
+                out2: for (int u = 1; (long) u * u <= LIMIT; u++) {
+                    int[] forward = new int[26];
+                    char[] backward = new char[10];
+                    for (int k = 0; k < 26; k++) {
+                        forward[k] = -1;
+                    }
+
+                    long v = (long) u * u;
+                    for (int k = n - 1; k >= 0; k--, v /= 10) {
+                        if (backward[(int) (v % 10)] == '\0') {
+                            forward[a[i].charAt(k) - 'A'] = (int) (v % 10);
+                            backward[(int) (v % 10)] = a[i].charAt(k);
+                        } else if (backward[(int) (v % 10)] != a[i].charAt(k)) {
+                            continue out2;
+                        }
+                    }
+
+                    if (forward[a[i].charAt(0) - 'A'] == 0 || forward[a[j].charAt(0) - 'A'] == 0 || v != 0) {
+                        continue;
+                    }
+
+                    long x = 0;
+                    for (int k = 0; k < n; k++) {
+                        x = 10 * x + forward[a[j].charAt(k) - 'A'];
+                    }
+
+                    long w = (long) Math.sqrt(x);
+                    if (w * w == x) {
+                        ans = Math.max(ans, (long) u * u);
+                        ans = Math.max(ans, x);
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
---
+++ src/solutions/_000/Problem099.java
@@ -0,0 +1,30 @@
+package solutions._000;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Problem099 {
+    private static final int n = 1000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/input/p099_base_exp.txt"));
+        int[][] a = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            String[] split = br.readLine().split(",");
+            a[i][0] = Integer.parseInt(split[0]);
+            a[i][1] = Integer.parseInt(split[1]);
+        }
+
+        double max = 0;
+        int ans = 0;
+        for (int i = 0; i < a.length; i++) {
+            double log = a[i][1] * Math.log(a[i][0]);
+            if (log > max) {
+                max = log;
+                ans = i;
+            }
+        }
+        System.out.println(ans + 1);
+    }
+}
